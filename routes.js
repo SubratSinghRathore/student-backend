4d0ca9b175fb73443e5329ecb80a5acfd57b1e62
subrat singh <subratprivate777@gmail.com>
2025-02-21 14:56:50 +0530

    a

--- routes.js
+++ routes.js
@@ -12,14 +12,14 @@ router.post('/login', async (req, res) => {
     const mail = req.body.email;
     const password = req.body.pass;
     try {
-        const Login = await login.findOne({
-            db_mail,
-            db_pass
+        const Login = await register.findOne({
+            email,
+            pass
         });
         if (Login == null) {
             res.status(400).send("invalid credentials");
         }
-        else if (mail == Login.db_mail && Login.password == db_pass) {
+        else if (mail == Login.email && Login.pass == password) {
             const link_url = Login.url;
             res.status(200).send(`<!DOCTYPE html>
 <html lang="en">
@@ -30,8 +30,8 @@ router.post('/login', async (req, res) => {
 </head>
 <body style="font-family: Arial, sans-serif; text-align: center; margin: 50px; background-color: #f4f4f4;">
     <div style="background: white; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); display: inline-block;">
-        <h2>Logged in as anu pria.</h2>
-        <p>Please log in again with your credentials.</p>
+        <h2>Welcome ${login.name}</h2>
+        <p></p>
         <a href="https://student-learning-platform-1uoa.vercel.app/" style="display: inline-block; margin-top: 20px; padding: 10px 20px; font-size: 16px; color: white; background: #007BFF; text-decoration: none; border-radius: 5px;">Go to Login</a>
     </div>
 </body>
