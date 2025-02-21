c468cd3c048eebc64d326b07f91856cc6dd7dea0
subrat singh <subratprivate777@gmail.com>
2025-02-22 02:03:42 +0530

    done

--- routes.js
+++ routes.js
@@ -41,46 +41,29 @@ router.use(Router.json());
 //         res.status(400).send(error);
 //     }
 // });
-router.post('/login', async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.pass;
+router.get('/login', async (req, res) => {
+    const email = req.query.email;
+    const password = req.query.pass;
     console.log(email);
 
-//     try {
-//         const user = await register.findOne({ email });
+    try {
+        const user = await register.findOne({ email });
+        console.log(user);
 
-//         if (!user) {
-//             return res.status(400).send("Invalid credentials");
-//         }
+        if (user == null) {
+            return res.status(400).send("Invalid credentials");
+        }
 
-//         if (user.pass === password) { // Use bcrypt.compareSync(password, user.pass) if hashed passwords are used
-//             const link_url = user.url;
-//             return res.status(200).send(`<!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>Logged In</title>
-// </head>
-// <body style="font-family: Arial, sans-serif; text-align: center; margin: 50px; background-color: #f4f4f4;">
-//     <div style="background: white; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); display: inline-block;">
-//         <h2>Welcome </h2>
-//         <p></p>
-//         <a href="https://student-learning-platform-1uoa.vercel.app/" 
-//            style="display: inline-block; margin-top: 20px; padding: 10px 20px; font-size: 16px; color: white; 
-//                   background: #007BFF; text-decoration: none; border-radius: 5px;">
-//            Go to Dashboard
-//         </a>
-//     </div>
-// </body>
-// </html>`);
-//         } else {
-//             return res.status(400).send("Invalid email or password");
-//         }
-//     } catch (error) {
-//         console.error(error);
-//         return res.status(500).send("Internal Server Error");
-//     }
+        if (user.pass === password) { // Use bcrypt.compareSync(password, user.pass) if hashed passwords are used
+            
+            return res.status(200).json({username: user.name});
+        } else {
+            return res.status(400).send("Invalid email or password");
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Internal Server Error");
+    }
 });
 
 
